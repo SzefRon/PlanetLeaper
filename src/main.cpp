8a4cfec9d2e2be3e4edfe2f74e0fa7c580323703
SzefRon <ziome550@wp.pl>
2025-02-14 19:24:16 +0100

    zooming

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,12 @@
 #include <chrono>
 #include <Windows.h>
 
+const int screen_x = 128, screen_y = 64;
+
 bool within_bounds(glm::ivec2 vec)
 {
     if (vec.x >= 0 && vec.y >= 0
-    && vec.x < 16 && vec.y < 16) {
+    && vec.x < screen_x && vec.y < screen_y) {
         return true;
     }
     return false;
@@ -18,13 +20,14 @@ int main()
     float camera_position_x = 0.0f;
     float camera_position_y = 0.0f;
     float camera_rotation = 0.0f;
+    float camera_zoom = 1.0f;
 
-    char screen[16][16];
-    std::fill(screen[0], screen[15] + 16, ' ');
+    char screen[screen_y][screen_x];
+    std::fill(screen[0], screen[screen_y] + screen_x, ' ');
 
     while (true) {
         std::cout << "\033[H";
-        std::fill(screen[0], screen[15] + 16, ' ');
+        std::fill(screen[0], screen[screen_y] + screen_x, ' ');
 
         if (GetAsyncKeyState(VkKeyScan('w'))) camera_position_y += 0.1f;
         if (GetAsyncKeyState(VkKeyScan('s'))) camera_position_y -= 0.1f;
@@ -34,14 +37,18 @@ int main()
         if (GetAsyncKeyState(VkKeyScan('q'))) camera_rotation += 0.01f;
         if (GetAsyncKeyState(VkKeyScan('e'))) camera_rotation -= 0.01f;
 
+        if (GetAsyncKeyState(VkKeyScan('z'))) camera_zoom -= 0.01f;
+        if (GetAsyncKeyState(VkKeyScan('c'))) camera_zoom += 0.01f;
+
         glm::mat4 view = glm::mat4(1.0f);
-        view = glm::translate(view, glm::vec3(8.0f, 8.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(screen_x * 0.5f, screen_y * 0.5f, 0.0f));
+        view = glm::scale(view, glm::vec3(camera_zoom, camera_zoom, 1.0f));
         view = glm::rotate(view, -camera_rotation, glm::vec3(0.0f, 0.0f, 1.0f));
-        view = glm::translate(view, glm::vec3(-8.0f, -8.0f, 0.0f));
+        view = glm::translate(view, glm::vec3(-screen_x * 0.5f, -screen_y * 0.5f, 0.0f));
         view = glm::translate(view, glm::vec3(camera_position_x, camera_position_y, 0.0f));
 
-        for (int i = 0; i < 8; i++) {
-            for (int j = 0; j < 8; j++) {
+        for (int i = 0; i < 32; i++) {
+            for (int j = 0; j < 32; j++) {
 
                 glm::vec4 pos = {i, j, 0.0f, 1.0f};
                 glm::vec4 screen_pos = view * pos;
@@ -59,14 +66,15 @@ int main()
         }
 
         std::string screen_final;
-
-        for (int i = 0; i < 16; i++) {
-            for (int j = 0; j < 16; j++) {
-                std::cout << screen[i][j];
+        screen_final.reserve(screen_y * (screen_x + 1));
+        for (int i = 0; i < screen_y; i++) {
+            for (int j = 0; j < screen_x; j++) {
+                screen_final.push_back(screen[i][j]);
             }
-            std::cout << '\n';
+            screen_final.push_back('\n');
         }
 
+        std::cout << screen_final;
         std::cout << camera_position_x << " " << camera_position_y;
     }
 
